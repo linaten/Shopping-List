aa7f2219a4f3124f6ba5a778d3b26068fea1859a
Lindsey Atencio <lindsey.atencio@gmail.com>
2019-10-04 10:09:35 -0700

    prework shopping list

--- script.js
+++ script.js
@@ -2,28 +2,51 @@ window.onload = function () {
 	initShoppingList();
 };
 function initShoppingList() {
-	let form = document.getElmentById("item-form")
+	let form = document.getElementById("item-form");
 	form.addEventListener("submit", (event) => {
 		handleItemForm(event, form);
 	});
 }
 
 function handleItemForm(event, formRef) {
-if(event.preventDefaukt()) {
-	event.preventDefault();
+	if(event.preventDefault) {
+		event.preventDefault();
 	}
-let itemHtml = addItemToShoppingList();
-let itemListRef = document.getElementById("shopping-list");
-itemListRef.insertAdjacentHTML("afterend", itemHTML)
-return false;
+	addItemToShoppingList();
+	formRef.reset();
+
+	return false;
 }
+
 function addItemToShoppingList() {
-	let itemName= document.getElementById("item-name");
-	let itemAmount= document.getElementById("item-amount");
+	let itemName = document.getElementById("item-name");
+	let itemAmount = document.getElementById("item-amount");
+	let id = getRandomInt (0,100000);
+
+	let itemHtml = createListItemHtml(itemName.value, itemAmount.value, id);
+	let itemListRef = document.getElementById("shopping-list");
+	itemListRef.insertAdjacentHTML("afterend", itemHtml);
+	setDeleteButtonEvent(id);
 }
-function createListItemHtml() {
-	return '<li>
-		Item Name - Amount
+function setDeleteButtonEvent(id) {
+let deleteButton = document.getElementById("button"+id);
+deleteButton.addEventListener("click", () => {
+	removeListItem (id);
+});
+}
+function createListItemHtml(itemName, itemAmount, id) {
+	return `<li id="item${id}">
+		${itemName} - ${itemAmount}
+		<button id="button${id}" type="button">Delete Item</button>
 	</li>
-	';
+	`;
 }
+function removeListItem(id) {
+	let listItem = document.getElementById("item" + id);
+	listItem.parentNode.removeChild (listItem);
+}
+	function getRandomInt(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max-min)) + min;
+		}
